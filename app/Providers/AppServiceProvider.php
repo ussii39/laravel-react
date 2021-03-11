fddaf6b36a58f6a1785f76c81a94c55b2759fecc
ussii39 <usimarutetunori3@outlook.jp>
2021-03-12 00:06:30 +0900

    https化

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,6 +23,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        Schema::defaultStringLength(191);
+        // Schema::defaultStringLength(191);
+        if (\App::environment(['production'])) {
+            \URL::forceScheme('https');
+        }
     }
 }
